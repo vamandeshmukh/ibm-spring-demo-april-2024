d2df2ef71a66cf1929a98c6467bc4439cce2383f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-15 16:40:50 +0530

    spring basics

--- src/main/java/com/ibm/demo/JavaConfigDemo.java
+++ src/main/java/com/ibm/demo/JavaConfigDemo.java
@@ -5,6 +5,9 @@ public class JavaConfigDemo {
 	public static void main(String[] args) {
 		
 		// write necessary code here and in Employee class 
+		
+		// read page 30 onwards - 
+		// https://docs.google.com/document/d/1gjd0EmZKpZsIGuU7zTre9IZaLqHeqwjyQtUixNDbIVU/edit 
 
 		
 	}
