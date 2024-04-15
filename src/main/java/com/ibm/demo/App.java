7c1b42b0a228dd7ca608290b44c9369bd89f0bc5
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-15 12:21:11 +0530

    spring

---
+++ src/main/java/com/ibm/demo/App.java
@@ -0,0 +1,12 @@
+package com.ibm.demo;
+
+/**
+ * @author Vaman Deshmukh
+ * @version 1.0.0
+ */
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
