75652aef5e2064cd9b78a6841a1492c29ed5d27f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-15 15:10:57 +0530

    singleton

--- src/main/java/com/ibm/demo/SpringContextDemo.java
+++ src/main/java/com/ibm/demo/SpringContextDemo.java
@@ -1,16 +1,24 @@
 package com.ibm.demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.ibm.demo.model.Employee;
 
 public class SpringContextDemo {
 
 	public static void main(String[] args) {
 
-		Employee emp = new Employee();
+		ApplicationContext context = new ClassPathXmlApplicationContext();
+
+//		Employee emp = new Employee();
+		Employee emp = context.getBean(Employee.class);
 
 		System.out.println(emp.hashCode());
+
+		((AbstractApplicationContext) context).close();
 
 	}
 
 }
-
